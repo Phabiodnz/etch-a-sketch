703951dfbcd779fd9c726e05199cc90b5936b6d7
Phabiodnz <Phabiodr@hotmail.com>
2024-05-19 16:22:08 -0300

    Lógica da grid e estrutura do site atualizada

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-let quantidadeDivs = 16 * 16;
-const gridContainerJS = document.querySelector('.grid-container')
+const redefineButtonJS = document.querySelector('.redefine-button');
+const updateButtonJS = document.querySelector('.update-button');
+const gridContainerJS = document.querySelector('.grid-container');
 
-for (let i = 0; i < quantidadeDivs; i++) {
-    let novaDiv = document.createElement("div");
-    novaDiv.classList.add("grid-blocks");
-    novaDiv.addEventListener('mouseover', (evento) =>{
-        novaDiv.style.backgroundColor = 'black';
-    });
-    gridContainerJS.appendChild(novaDiv);
+let userImput = prompt('Choose your grid size (1~80):');
+
+function gridCreation(divQuantity){
+    if ((divQuantity >= 1 && divQuantity <= 80)){
+        gridContainerJS.innerHTML = '';
+        let quantityCalculation = divQuantity ** 2;
+        let blockWidthHeight = Math.floor(960/ divQuantity);
+
+        for (let i = 0; i < quantityCalculation; i++) {
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("grid-blocks");
+            newDiv.addEventListener('mouseover', (evento) =>{
+                newDiv.style.backgroundColor = 'black';
+            });     
+            gridContainerJS.appendChild(newDiv);
+        }
+        
+        let divConfig = document.querySelectorAll(".grid-blocks");
+        divConfig.forEach(function(div) {
+            div.style.width = blockWidthHeight + "px";
+            div.style.height = blockWidthHeight + "px";
+        });
+    }else{
+        alert("Invalid Imput");
+    }
 }
 
+updateButtonJS.addEventListener('click', updateGrid);
+
+function updateGrid() {
+    let inputUpdate = prompt('Choose your grid size (1~80):');
+    gridCreation(Number(inputUpdate));
+}
+
+
+redefineButtonJS.addEventListener("click", function() {
+    let redefineDivs = document.querySelectorAll(".grid-blocks");
+    redefineDivs.forEach(function(div) {
+        div.style.backgroundColor = 'white';
+    });
+});
+
+gridCreation(userImput);
+console.log(updateButtonJS);
